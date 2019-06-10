242d6e154752aba7dec20afc153a86621135110f
Reza Mesgaran <reza_mesgaran@live.com>
2019-06-10 19:42:21 -0400

    Add project files.

---
+++ Console_ReadOnly-constant/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_ReadOnly_constant
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+    class person
+    {
+        const double PI = 3.14;
+        readonly int myReadonlyField = 10;
+
+        //-------------------- First Difference
+
+        // const myConstField;  ERROR -> constant field must be initialized when it is declare
+        readonly int myReadonlyField2;    // readonly field can be declared without initialization
+
+    }
+}
