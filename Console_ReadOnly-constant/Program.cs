c9a56e9acf4a3c801251520b48a146b3384bfcad
Reza Mesgaran <reza_mesgaran@live.com>
2019-06-10 19:53:43 -0400

    The Third difference between Readonly fields and Const fields

--- Console_ReadOnly-constant/Program.cs
+++ Console_ReadOnly-constant/Program.cs
@@ -10,7 +10,6 @@ namespace Console_ReadOnly_constant
     {
         static void Main(string[] args)
         {
-
         }
     }
     class person
@@ -21,15 +20,23 @@ namespace Console_ReadOnly_constant
         //-------------------- First Difference
 
         // const myConstField;  ERROR -> constant field must be initialized when it is declare
-        readonly int myReadonlyField2;    // readonly field can be declared without initialization
+        public readonly int myReadonlyField2;    // readonly field can be declared without initialization
 
 
-        person()   //constructor
+        public person()   //constructor
         {
             //-------------------- Second Difference
 
             myReadonlyField = 10 + 10;  // readonly fields's value can change ONLY in Constructor
                                         // PI = 100;   ERROR -> Const fields's value can not be change anymore
         }
+
+        //-------------------- Third Difference  
+
+        const double myConstField2 = Math.Sqrt(4);  // Const field can NOT be assigned a value that is a result of a calculation
+        readonly double myReadonlyField3 = Math.Sqrt(4);  // readonly field can be assigned a value that is a result of a calculation
+
+
+
     }
 }
